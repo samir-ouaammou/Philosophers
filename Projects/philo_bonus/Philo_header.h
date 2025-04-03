021646b36c1760bd52f6500e684aad48cb8e371b
Samir Ouaammou <souaammo@c3r9p3.1337.ma>
2025-04-03 14:45:05 +0000

    update all

--- Projects/philo_bonus/Philo_header.h
+++ Projects/philo_bonus/Philo_header.h
@@ -1,54 +1,54 @@
 #ifndef PHILO_HEADER_H
-#define PHILO_HEADER_H
+# define PHILO_HEADER_H
 
-#include <stdio.h>
-#include <fcntl.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <signal.h>
-#include <pthread.h>
-#include <sys/wait.h>
-#include <sys/time.h>
-#include <semaphore.h>
-#include "malloc/malloc.h"
+# include "malloc/malloc.h"
+# include <fcntl.h>
+# include <pthread.h>
+# include <semaphore.h>
+# include <signal.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <sys/time.h>
+# include <sys/wait.h>
+# include <unistd.h>
 
-typedef struct s_data t_data;
+typedef struct s_data	t_data;
 
 typedef struct s_philo
 {
-    pid_t           pid;
-    size_t         id;
-    size_t         time_to_die;
-    size_t         time_to_eat;
-    size_t         time_to_sleep;
-    ssize_t         num_to_eat;
-    size_t         last_meal;
-    t_data          *data;
-} t_philo;
+	pid_t				pid;
+	size_t				id;
+	size_t				time_to_die;
+	size_t				time_to_eat;
+	size_t				time_to_sleep;
+	ssize_t				num_to_eat;
+	size_t				last_meal;
+	t_data				*data;
+}						t_philo;
 
 typedef struct s_data
 {
-    size_t         i;
-    size_t         num_philos;
-    size_t         time_to_die;
-    size_t         time_to_eat;
-    size_t         time_to_sleep;
-    ssize_t         num_to_eat;
-    size_t         start_time;
-    sem_t           *forks;
-    sem_t           *print;
-    sem_t           *monitor;
-    t_philo         *philos;
-} t_data;
+	size_t				i;
+	size_t				num_philos;
+	size_t				time_to_die;
+	size_t				time_to_eat;
+	size_t				time_to_sleep;
+	ssize_t				num_to_eat;
+	size_t				start_time;
+	sem_t				*forks;
+	sem_t				*print;
+	sem_t				*monitor;
+	t_philo				*philos;
+}						t_data;
 
-void ft_usleep(size_t time_to_sleep);
-size_t ft_get_current_time(void);
-void    ft_philo_init(t_data *data);
-void    ft_philo_pars(t_data *data, int ac, char **av);
-int ft_atoi(const char *str);
-void ft_print_usage_and_exit(char *msg);
-void    ft_print_status(t_philo *philo, char *status);
-void ft_philosopher_routine(t_philo *philo);
-void    ft_philo_one(t_philo *philo);
+int						ft_atoi(const char *str);
+size_t					ft_get_current_time(void);
+void					ft_philo_init(t_data *data);
+void					ft_philo_one(t_philo *philo);
+void					ft_usleep(size_t time_to_sleep);
+void					ft_print_usage_and_exit(char *msg);
+void					ft_philosopher_routine(t_philo *philo);
+void					ft_print_status(t_philo *philo, char *status);
+void					ft_philo_pars(t_data *data, int ac, char **av);
 
 #endif
--- Projects/philo_bonus/Philo_parsing.c
+++ Projects/philo_bonus/Philo_parsing.c
@@ -1,88 +1,93 @@
 #include "Philo_header.h"
 
-void ft_print_usage_and_exit(char *msg)
+void	ft_print_usage_and_exit(char *msg)
 {
-    if (!msg)
-    {
-        printf("Usage: ./philo number_of_philosophers time_to_die time_to_eat ");
-        printf("time_to_sleep [number_of_times_each_philosopher_must_eat]\n");
-        printf("Note: All arguments must be positive integers between 1 and 2147483647.\n");
-        printf("Error: The program requires 4 or 5 arguments.\n");
-    }
-    else
-        printf("Error: %s\n", msg);
-    ft_exit(EXIT_FAILURE);
+	if (!msg)
+	{
+		printf("Usage: ./philo number_of_philosophers ");
+		printf("time_to_die time_to_eat time_to_sleep ");
+		printf("[number_of_times_each_philosopher_must_eat]\n");
+		printf("Note: All arguments must be positive ");
+		printf("integers between 1 and 2147483647.\n");
+		printf("Error: The program requires 4 or 5 arguments.\n");
+	}
+	else
+		printf("Error: %s\n", msg);
+	ft_exit(EXIT_FAILURE);
 }
 
-int ft_atoi(const char *str)
+int	ft_atoi(const char *str)
 {
-    short   i;
-    size_t  res;
-    
-    i = 0;
-    res = 0;
-    if (!str || !str[0])
-        ft_print_usage_and_exit("All arguments must be positive integers.");
-    if (str[i] == '+')
-        i++;
-    else if (str[i] == '-')
-        ft_print_usage_and_exit("All arguments must be positive integers.");
-    while (str[i] && (str[i] >= '0' && str[i] <= '9'))
-    {
-        res = res * 10 + (str[i] - '0');
-        if (res > 2147483647)
-            ft_print_usage_and_exit("All arguments must be positive integers.");
-        i++;
-    }
-    if (str[i] != '\0')
-        ft_print_usage_and_exit("All arguments must be positive integers.");
-    return ((int)res);
+	short	i;
+	size_t	res;
+
+	i = 0;
+	res = 0;
+	if (!str || !str[0])
+		ft_print_usage_and_exit("All arguments must be positive integers.");
+	if (str[i] == '+')
+		i++;
+	else if (str[i] == '-')
+		ft_print_usage_and_exit("All arguments must be positive integers.");
+	while (str[i] && (str[i] >= '0' && str[i] <= '9'))
+	{
+		res = res * 10 + (str[i] - '0');
+		if (res > 2147483647)
+			ft_print_usage_and_exit("All arguments must be positive integers.");
+		i++;
+	}
+	if (str[i] != '\0')
+		ft_print_usage_and_exit("All arguments must be positive integers.");
+	return ((int)res);
 }
 
-void    ft_philo_pars(t_data *data, int ac, char **av)
+void	ft_philo_pars(t_data *data, int ac, char **av)
 {
-    if (!data || !ac || !av)
-        ft_print_usage_and_exit("ft_philo_pars arguments.");
-    data->num_philos = ft_atoi(av[1]);
-    data->time_to_die = ft_atoi(av[2]);
-    data->time_to_eat = ft_atoi(av[3]);
-    data->time_to_sleep = ft_atoi(av[4]);
-    if (av[5])
-        data->num_to_eat = ft_atoi(av[5]);
-    else
-        data->num_to_eat = -1;
-    if (data->num_philos < 1 || data->num_philos > 200)
-        ft_print_usage_and_exit("Number of philosophers must be between 1 and 200.");
-    if (data->time_to_die < 60) 
-        ft_print_usage_and_exit("time_to_die must be at least 60 ms.");
-    if (data->time_to_eat < 60) 
-        ft_print_usage_and_exit("time_to_eat must be at least 60 ms.");
-    if (data->time_to_sleep < 60)
-        ft_print_usage_and_exit("time_to_sleep must be at least 60 ms.");
-    if (data->num_to_eat == 0)
-        ft_exit(0);
+	if (!data || !ac || !av)
+		ft_print_usage_and_exit("ft_philo_pars arguments.");
+	data->num_philos = ft_atoi(av[1]);
+	data->time_to_die = ft_atoi(av[2]);
+	data->time_to_eat = ft_atoi(av[3]);
+	data->time_to_sleep = ft_atoi(av[4]);
+	if (av[5])
+		data->num_to_eat = ft_atoi(av[5]);
+	else
+		data->num_to_eat = -1;
+	if (data->num_philos < 1 || data->num_philos > 200)
+	{
+		printf("Number of philosophers ");
+		ft_print_usage_and_exit("must be between 1 and 200.");
+	}
+	if (data->time_to_die < 60)
+		ft_print_usage_and_exit("time_to_die must be at least 60 ms.");
+	if (data->time_to_eat < 60)
+		ft_print_usage_and_exit("time_to_eat must be at least 60 ms.");
+	if (data->time_to_sleep < 60)
+		ft_print_usage_and_exit("time_to_sleep must be at least 60 ms.");
+	if (data->num_to_eat == 0)
+		ft_exit(0);
 }
 
-void    ft_philo_init(t_data *data)
+void	ft_philo_init(t_data *data)
 {
-    size_t   i;
+	size_t	i;
 
-    data->forks = sem_open("/forks", O_CREAT, 0644, data->num_philos);
-    data->print = sem_open("/print", O_CREAT, 0644, 1);
-    data->monitor = sem_open("/monitor", O_CREAT, 0644, 1);
-    data->philos = ft_malloc(data->num_philos * sizeof(t_philo));
-    data->start_time = ft_get_current_time();
-    data->i = 0;
-    i = 0;
-    while (i < data->num_philos)
-    {
-        data->philos[i].id = i + 1;
-        data->philos[i].time_to_die = data->time_to_die;
-        data->philos[i].time_to_eat = data->time_to_eat;
-        data->philos[i].time_to_sleep = data->time_to_sleep;
-        data->philos[i].num_to_eat = data->num_to_eat;
-        data->philos[i].last_meal = data->start_time;
-        data->philos[i].data = data;
-        i++;
-    }
+	data->forks = sem_open("/forks", O_CREAT, 0644, data->num_philos);
+	data->print = sem_open("/print", O_CREAT, 0644, 1);
+	data->monitor = sem_open("/monitor", O_CREAT, 0644, 1);
+	data->philos = ft_malloc(data->num_philos * sizeof(t_philo));
+	data->start_time = ft_get_current_time();
+	data->i = 0;
+	i = 0;
+	while (i < data->num_philos)
+	{
+		data->philos[i].id = i + 1;
+		data->philos[i].time_to_die = data->time_to_die;
+		data->philos[i].time_to_eat = data->time_to_eat;
+		data->philos[i].time_to_sleep = data->time_to_sleep;
+		data->philos[i].num_to_eat = data->num_to_eat;
+		data->philos[i].last_meal = data->start_time;
+		data->philos[i].data = data;
+		i++;
+	}
 }
